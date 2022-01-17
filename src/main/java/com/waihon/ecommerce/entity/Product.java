d66e2a8b93f5cff335d93836a42f579c26cc7ad0
Waihon Yew <yewwaihon@gmail.com>
2022-01-17 23:47:54 +0800

    Annotate properties with @Column and other relevant annotations

--- src/main/java/com/waihon/ecommerce/entity/Product.java
+++ src/main/java/com/waihon/ecommerce/entity/Product.java
@@ -1,7 +1,10 @@
 package com.waihon.ecommerce.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -12,14 +15,37 @@ import java.util.Date;
 @Data
 
 public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
+
+	@Column(name = "sku")
 	private String sku;
+
+	@Column(name = "name")
 	private String name;
+
+	@Column(name = "description")
 	private String description;
+
+	@Column(name = "unit_price")
 	private BigDecimal unitPrice;
+
+	@Column(name = "image_url")
 	private String imageUrl;
+
+	@Column(name = "active")
 	private boolean active;
+
+	@Column(name = "units_in_stock")
 	private int unitsInStock;
+
+	@Column(name = "date_created")
+	@CreationTimestamp
 	private Date dateCreated;
+
+	@Column(name = "last_updated")
+	@UpdateTimestamp
 	private Date lastUpdated;
 }
