27bfeb210b37a58aeddda7833dc94aeeeea8efca
Waihon Yew <yewwaihon@gmail.com>
2022-01-17 23:07:55 +0800

    Initial Maven project generated by Spring initializr

---
+++ src/test/java/com/waihon/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waihon.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
